848c6f8826c351f7ce82571a4027413c6691c429
MyraTy <113852108+MyraTy@users.noreply.github.com>
2023-01-03 11:42:08 +0100

    Overwrite while reading because of the four lines of delete_all

--- data/lib/gestorntdb/__init__.py
+++ data/lib/gestorntdb/__init__.py
@@ -14,7 +14,12 @@ def writenotes(user, dir_, note, remember=None, overwrite=False):
         notes = open("C:/Users/{user}/{dir_}/Sticky Notes/data/notes/notes.ntdb".format(user=user, dir_=dir_), mode="w", encoding="utf8")
     else:
         notes = open("C:/Users/{user}/{dir_}/Sticky Notes/data/notes/notes.ntdb".format(user=user, dir_=dir_), mode="a", encoding="utf8")
-
+        
+    text = notes.read()
+    if text = "||||":
+        notes.close()
+        notes = open("C:/Users/{user}/{dir_}/Sticky Notes/data/notes/notes.ntdb".format(user=user, dir_=dir_), mode="w", encoding="utf8")
+        
     if remember == None:
         if overwrite:
             notes.write("Genesis Note. Only for pad." + "||||" + note)
