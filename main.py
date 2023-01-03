8f563a5273e8bd68537154964fd8a6d14bc0c44f
MyraTy <113852108+MyraTy@users.noreply.github.com>
2023-01-03 11:38:58 +0100

    Initial Commit

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import data.lib.gestorntdb as pinnote
 
 print("""Run in idle.
 Aviable functions: writenotes, readnotes, delete_all.
+Call that functions using pinnote.<insert here name of function>()
 More information when you call that fuctions.""")
